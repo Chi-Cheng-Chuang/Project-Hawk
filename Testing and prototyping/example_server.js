9ac0ca6c7d86ad29d93a37d76d12bf0522c6ce74
garyfishel <gary.fishel@wsu.edu>
2023-04-18 15:15:45 -0700

    updated repo for UI changes

--- Testing and prototyping/example_server.js
+++ Testing and prototyping/example_server.js
@@ -1,120 +1,22 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const PORT = 8000;
+
+var express = require('express');
+var app = express();
+
 const cors = require("cors");
-const path = require('path');
-const PORT1 = 8000;
-const PORT2 = 9000;
+app.use(cors({
+    origin: 'null'
+}));
 
+app.use(express.static('.'));
 
-http.createServer(function (req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'null');
-  console.log(`${req.method} ${req.url}`);
+//app.use('.', express.static(__dirname + '.'));
 
-  // parse URL
-  const parsedUrl = url.parse(req.url);
-  // extract URL path
-  let pathname = `.${parsedUrl.pathname}`;
-  // based on the URL path, extract the file extension. e.g. .js, .doc, ...
-  const ext = path.parse(pathname).ext;
-  // maps file extension to MIME typere
-  const map = {
-    '.ico': 'image/x-icon',
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.json': 'application/json',
-    '.css': 'text/css',
-    '.png': 'image/png',
-    '.jpg': 'image/jpeg',
-    '.wav': 'audio/wav',
-    '.mp3': 'audio/mpeg',
-    '.svg': 'image/svg+xml',
-    '.pdf': 'application/pdf',
-    '.doc': 'application/msword'
-  };
+app.get('.', function(req, res) {
+    res.sendFile(path.join(public, '.'));
+});
 
-  fs.exists(pathname, function (exist) {
-    if(!exist) {
-      // if the file is not found, return 404
-      res.statusCode = 404;
-      res.end(`File ${pathname} not found!`);
-      return;
-    }
+var server = app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
 
-    // if is a directory search for index file matching the extension
-    if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
-
-    // read file from file system
-    fs.readFile(pathname, function(err, data){
-      if(err){
-        res.statusCode = 500;
-        res.end(`Error getting the file: ${err}.`);
-      } else {
-        // if the file is found, set Content-type and send data
-        res.setHeader('Content-type', map[ext] || 'text/plain' );
-        res.end(data);
-      }
-    });
-  });
-
-
-}).listen(parseInt(PORT1));
-
-http.createServer(function (req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'null');
-  console.log(`${req.method} ${req.url}`);
-
-  // parse URL
-  const parsedUrl = url.parse(req.url);
-  // extract URL path
-  let pathname = `.${parsedUrl.pathname}`;
-  // based on the URL path, extract the file extension. e.g. .js, .doc, ...
-  const ext = path.parse(pathname).ext;
-  // maps file extension to MIME typere
-  const map = {
-    '.ico': 'image/x-icon',
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.json': 'application/json',
-    '.css': 'text/css',
-    '.png': 'image/png',
-    '.jpg': 'image/jpeg',
-    '.wav': 'audio/wav',
-    '.mp3': 'audio/mpeg',
-    '.svg': 'image/svg+xml',
-    '.pdf': 'application/pdf',
-    '.doc': 'application/msword'
-  };
-
-  fs.exists(pathname, function (exist) {
-    if(exist) {
-      try {
-        fs.unlinkSync('data.csv');
-
-        console.log("Delete File successfully.");
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    // if is a directory search for index file matching the extension
-    if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
-
-    // read file from file system
-    fs.readFile(pathname, function(err, data){
-      if(err){
-        res.statusCode = 500;
-        res.end(`Error getting the file: ${err}.`);
-      } else {
-        // if the file is found, set Content-type and send data
-        res.setHeader('Content-type', map[ext] || 'text/plain' );
-        res.end(data);
-      }
-    });
-  });
-
-
-}).listen(parseInt(PORT2));
-
-console.log(`Server listening on port ${PORT1} and ${PORT2}`);
+//console.log('Listening on port 80');
 
--- Testing and prototyping/server.js
+++ Testing and prototyping/server.js
@@ -1,21 +1,119 @@
-const PORT = 8000;
-
-var express = require('express');
-var app = express();
-
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
 const cors = require("cors");
-app.use(cors({
-    origin: 'null'
-}));
+const path = require('path');
+const PORT1 = 8000;
+const PORT2 = 9000;
 
-app.use(express.static('.'));
 
-//app.use('.', express.static(__dirname + '.'));
+http.createServer(function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'null');
+  console.log(`${req.method} ${req.url}`);
 
-app.get('.', function(req, res) {
-    res.sendFile(path.join(public, '.'));
-});
+  // parse URL
+  const parsedUrl = url.parse(req.url);
+  // extract URL path
+  let pathname = `.${parsedUrl.pathname}`;
+  // based on the URL path, extract the file extension. e.g. .js, .doc, ...
+  const ext = path.parse(pathname).ext;
+  // maps file extension to MIME typere
+  const map = {
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.wav': 'audio/wav',
+    '.mp3': 'audio/mpeg',
+    '.svg': 'image/svg+xml',
+    '.pdf': 'application/pdf',
+    '.doc': 'application/msword'
+  };
 
-var server = app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+  fs.exists(pathname, function (exist) {
+    if(!exist) {
+      // if the file is not found, return 404
+      res.statusCode = 404;
+      res.end(`File ${pathname} not found!`);
+      return;
+    }
 
-//console.log('Listening on port 80');
+    // if is a directory search for index file matching the extension
+    if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
+
+    // read file from file system
+    fs.readFile(pathname, function(err, data){
+      if(err){
+        res.statusCode = 500;
+        res.end(`Error getting the file: ${err}.`);
+      } else {
+        // if the file is found, set Content-type and send data
+        res.setHeader('Content-type', map[ext] || 'text/plain' );
+        res.end(data);
+      }
+    });
+  });
+
+
+}).listen(parseInt(PORT1));
+
+http.createServer(function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'null');
+  console.log(`${req.method} ${req.url}`);
+
+  // parse URL
+  const parsedUrl = url.parse(req.url);
+  // extract URL path
+  let pathname = `.${parsedUrl.pathname}`;
+  // based on the URL path, extract the file extension. e.g. .js, .doc, ...
+  const ext = path.parse(pathname).ext;
+  // maps file extension to MIME typere
+  const map = {
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.wav': 'audio/wav',
+    '.mp3': 'audio/mpeg',
+    '.svg': 'image/svg+xml',
+    '.pdf': 'application/pdf',
+    '.doc': 'application/msword'
+  };
+
+  fs.exists(pathname, function (exist) {
+    if(exist) {
+      try {
+        fs.unlinkSync('data.csv');
+
+        console.log("Delete File successfully.");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    // if is a directory search for index file matching the extension
+    if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
+
+    // read file from file system
+    fs.readFile(pathname, function(err, data){
+      if(err){
+        res.statusCode = 500;
+        res.end(`Error getting the file: ${err}.`);
+      } else {
+        // if the file is found, set Content-type and send data
+        res.setHeader('Content-type', map[ext] || 'text/plain' );
+        res.end(data);
+      }
+    });
+  });
+
+
+}).listen(parseInt(PORT2));
+
+console.log(`Server listening on port ${PORT1} and ${PORT2}`);
